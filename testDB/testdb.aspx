e8675cf21a25d0a83115ed9c1ce4a51d3f362c79
IsaacAneek <81284474+IsaacAneek@users.noreply.github.com>
2026-05-25 00:58:04 +0600

    Add SQL Server 2019 instance and testDB script

---
+++ testDB/testdb.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestDB.aspx.cs" Inherits="YourProject.TestDB" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title>SQL Test</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Button ID="btnTest" runat="server" Text="Test DB Connection" OnClick="btnTest_Click" />
+
+            <br /><br />
+
+            <asp:Label ID="lblResult" runat="server" Font-Size="Large"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ testDB/testdb.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace YourProject
+{
+    public partial class TestDB : System.Web.UI.Page
+    {
+        protected void btnTest_Click(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT @@SERVERNAME", conn);
+                    string serverName = cmd.ExecuteScalar().ToString();
+
+                    lblResult.Text = "Connected Successfully! Server: " + serverName;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = "Connection Failed: " + ex.Message;
+            }
+        }
+    }
+}
