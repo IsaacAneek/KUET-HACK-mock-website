1d98622472bac96ef1c5a5d4db9aa0c8e58c2353
IsaacAneek <81284474+IsaacAneek@users.noreply.github.com>
2026-06-07 23:24:24 +0600

    Added DB to store username and password

--- login.aspx.cs
+++ login.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +18,42 @@ namespace kuet_hack_mock
         
         protected void onLoginClicked(object sender, EventArgs e)
         {
+            string inputUserName = username.Text.Trim();
+            string inputPassword = password.Text;
 
+            string connnectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+            string query = "INSERT INTO Users (Username, Password) VALUES (@Username, @Password)";
+
+            using (SqlConnection conn = new SqlConnection(connnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", inputUserName);
+                    cmd.Parameters.AddWithValue("@Password", inputPassword);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if(rowsAffected > 0)
+                        {
+                            Response.Write("<script>alert('Registration/Login recorded successfully!');</script>");
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        if(ex.Number == 2627)
+                        {
+                            // Unique constraint violation
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
